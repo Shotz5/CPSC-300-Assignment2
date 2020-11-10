2158aee05e20d78368dc2d4c4eb6d32f9a838370
Shotz5 <kitsulalex@gmail.com>
2020-11-09 16:22:40 -0800

    Added txt file input capability

--- MainWindow.cs
+++ MainWindow.cs
@@ -7,18 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Security;
 
 namespace Assignment_2
 {
-    public partial class MainWindow : Form
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Form {
+        public OpenFileDialog openFile;
+        public MainWindow() {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e) {
+        private void inputfile_Click(object sender, EventArgs e) {
+            openFile = new OpenFileDialog() {
+                FileName = "BankFile.txt",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open Bank Lineup File"
+            };
 
+            DialogResult result = openFile.ShowDialog();
+            if (result == DialogResult.OK) {
+                try {
+                    var filePath = openFile.FileName;
+                } catch (Exception eve) {
+                    MessageBox.Show($"{eve.Message}\n\n {eve.StackTrace}");
+                }
+            } else if (result == DialogResult.Cancel) {
+                MessageBox.Show("User cancelled Bank Line input");
+            }
         }
+
     }
 }
